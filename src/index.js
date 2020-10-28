0d8abb394fc47835a2d690545b8b5064fc5f4e00
piephai <tonphai2522@gmail.com>
2020-10-28 19:25:09 +1000

    Ignore warnings

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
+process.env.CI = false;
 
 ReactDOM.render(
   <React.StrictMode>
